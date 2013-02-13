0fe4b3048d51752577a276070a5480e2f88e531c
Anthony Liguori <aliguori@us.ibm.com>
2013-02-13 12:46:48 -0600

    Test markdown

---
+++ test.c
@@ -0,0 +1,11 @@
+/**
+ * Test of a `markdown` comment.
+ * Let's see if *bold* works.
+ * Let's see about an ![Code Monkey](http://chicktech.com/wp-content/uploads/2011/08/Code-Monkey.png)
+ * An [example](http://www.codemonkey.ws) link
+ */
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
